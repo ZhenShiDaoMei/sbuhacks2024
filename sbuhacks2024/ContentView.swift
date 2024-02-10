625f8d280d338d268fcb7869bd900466339f7bca
bry4nw0ng <bwong32104@gmail.com>
2024-02-10 16:56:55 -0500

    Initial Commit

---
+++ sbuhacks2024/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  sbuhacks2024
+//
+//  Created by Bryan on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sbuhacks2024/sbuhacks2024App.swift
@@ -0,0 +1,17 @@
+//
+//  sbuhacks2024App.swift
+//  sbuhacks2024
+//
+//  Created by Bryan on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct sbuhacks2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
