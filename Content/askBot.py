0c3467d1c232f119bbdbcb80601383c6bb9a9f0a
ZhenShiDaoMei <jasow211@gmail.com>
2024-02-11 03:58:32 -0500

    hopefully final flask update

--- Content/askBot.py
+++ Content/askBot.py
@@ -1,27 +1,106 @@
 from openai import OpenAI
 import gradio
 
-client = OpenAI(
-    api_key = "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"
-)
+count = 2
+tempString = ""
+words = []
+mystery_blurb = ""
+correct_guess = "Nice guess that was the missing word keep going!\n"
+no_no_word = ""
 
-css_path = './Content/askBot.css'
-with open(css_path, 'r') as css_file:
-    custom_css = css_file.read()
+def replace_first_occurrence_with_underscores(input_string, words_list):
+    output_string = input_string[0]
+    substring = input_string[1:]
+    
+    for word in words_list:
+        start_index = substring.find(word)
+        if start_index != -1:  # Word found
+            before_word = substring[:start_index]
+            after_word = substring[start_index + len(word):]
+            substring = before_word + '_' * len(word) + after_word
+
+    output_string += substring
+    return output_string
+
+def replace_first_underscore_sequence(input_string, first_word):
+    word_length = len(first_word)
+    output_string = input_string[0]
+    substring = input_string[1:] 
+    underscore_sequence = '_' * word_length
+    start_index = substring.find(underscore_sequence) 
+    if start_index != -1:
+        before_sequence = substring[:start_index]
+        after_sequence = substring[start_index + word_length:]
+        substring = before_sequence + first_word + after_sequence
+
+    output_string += substring
+    return output_string
+
+client = OpenAI(
+    api_key = "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"
+)
 
 messages = [{"role": "system", "content": ""}] #change content to whatever we want
 
 def CustomChatGPT(user_input):
-    messages.append({"role": "user", "content": user_input})
-    response = client.chat.completions.create(
-        model ="gpt-3.5-turbo",
-        messages = messages,
-        top_p=1,
-        temperature=0.9,
-        max_tokens=150
-    )
-    chatGPT_reply = response.choices[0].message.content
-    return chatGPT_reply
+    global mystery_blurb
+    global count
+    global tempString
+    global words
+    global correct_guess
+    global no_no_word
+    if count == 0:  
+        if user_input == words[0]:
+            mystery_blurb = replace_first_underscore_sequence(mystery_blurb, words[0])
+            if words.len() == 1:
+                count = 2
+                return mystery_blurb + "\nCongratulations you solved the the braindrAIn!, to keep on playing type a new topic."
+            words = words[1:]
+            return mystery_blurb + "\n" + correct_guess
+        else:
+            messages = [{"role": "system", "content": ""}]
+            messages.append({"role": "user", "content": "Give a hint that would help someone guess the the word: " + words[0]})
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages = messages,
+                top_p=1,
+                temperature=0.9,
+                max_tokens=150
+            )
+            chatGPT_reply = response.choices[0].message.content
+            return mystery_blurb + "\nWrong, Hint:" + chatGPT_reply + "\n" + words[0]
+        
+    if count == 1:
+        count = 0
+        messages = [{"role": "system", "content": "Previously you created this blurb: " + tempString + "(With this blurb we will play a game, listen to the rules carefully)"}]
+        messages.append({"role": "user", "content": "Remove 7 key words from the blurb and add them to a list (It can't be: " + no_no_word + "). Return the list such that its in the following format: List: word1, word2, word3, ..."})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages = messages,
+            top_p=1,
+            temperature=0.9,
+            max_tokens=150
+        )
+        chatGPT_reply = response.choices[0].message.content
+        words = chatGPT_reply.replace("List: ", "").split(", ")
+        mystery_blurb = replace_first_occurrence_with_underscores(tempString, words)
+        return mystery_blurb + "\n" + chatGPT_reply + words[0] + words[1]+words[2]+words[3]+words[4]
+    
+    if count == 2:
+        count = 1
+        message = [{"role": "system", "content": "You are a chatbot that will soon be given a topic to discuss. Provide a blurb that's  400 characters that contains some facts and random details."}]
+        message.append({"role": "user", "content": user_input})
+        response = client.chat.completions.create(
+            model ="gpt-3.5-turbo",
+            messages = message,
+            top_p=1,
+            temperature=0.9,
+            max_tokens=150
+        )
+        no_no_word = user_input
+        chatGPT_reply = response.choices[0].message.content
+        tempString = chatGPT_reply
+        return chatGPT_reply
 
 def chatgpt_clone(input,history):
     history = history or []
@@ -36,7 +115,7 @@ with gradio.Blocks() as demo:
     chatbot = gradio.Chatbot()
     msg = gradio.Textbox(placeholder="prompt")
     state = gradio.State() 
-    clear = gradio.ClearButton([msg, chatbot])
+    clear = gradio.ClearButton(msg)
     btn = gradio.Button("Submit")
     btn.click(fn=chatgpt_clone, inputs= [msg, state], outputs= [chatbot, state])
 
